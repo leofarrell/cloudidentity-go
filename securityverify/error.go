8f60454175a20a7c87c87ee5961dd265bd9f2a50
lmf <jimmythy@gmail.com>
2020-08-07 10:22:48 +1000

    Improve error handling for more detailed errors

--- securityverify/error.go
+++ securityverify/error.go
@@ -1,8 +1,12 @@
 package securityverify
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -16,10 +20,17 @@ func (sve SVError) Error() string {
 	return fmt.Sprintf("%s: %s", sve.ID, sve.Description)
 }
 
-// NewSVError will parse an error from Security verify. Or of no error message is returned will indicate the unexpected status recieved
+// NewSVError will parse an error from Security verify. Or if no error message is returned will indicate the unexpected status recieved
 func NewSVError(response *http.Response, expected int) error {
 	svError := &SVError{}
-	json.NewDecoder(response.Body).Decode(svError)
+	var buf bytes.Buffer
+	tee := io.TeeReader(response.Body, &buf)
+	json.NewDecoder(tee).Decode(svError)
+
+	// TODO some flag to turn extra verbose debug like this on/off.
+	log.Println("Unexpected body:")
+	erB, _ := ioutil.ReadAll(&buf)
+	log.Println(string(erB))
 
 	if svError.Description == "" {
 		return fmt.Errorf("Unexpected HTTP status %d, wanted %d", response.StatusCode, expected)
